74ca5f2db8fbfe7f4ac50c59e1925f19201ee12a
szakri <szabokristof2020@gmail.com>
2025-03-11 10:17:41 +0100

    Add base project

---
+++ Knorr-news/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Knorr_news.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
